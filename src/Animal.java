b8e871877e24b2bf2b31c32d131cb6fe199fb9d7
SimonaSI <stoicasimona19@stud.ase.ro>
2022-02-23 18:55:24 +0200

    Add all class

--- src/Animal.java
+++
@@ -1,5 +0,0 @@
-public abstract class Animal {
-    private String nume;
-
-    public abstract void mananca( String hrana);
-}
---
+++ src/pachet1/Animal.java
@@ -0,0 +1,19 @@
+package pachet1;
+
+public abstract class Animal {
+    private String nume;
+
+    public abstract void mananca( String hrana);
+
+    public Animal(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+}
