52c46ac7e76a641fd6ae2bc046f230004f46249c
SimonaSI <stoicasimona19@stud.ase.ro>
2022-03-02 18:57:37 +0200

    Add imbunatatiri2

---
+++ seminar2/src/clase/Program.java
@@ -0,0 +1,21 @@
+package clase;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		Reader angajatiReader = new AngajatiReader();
+		try {
+			listaAngajati = angajatiReader.readAplicanti("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
