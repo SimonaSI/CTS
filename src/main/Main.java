8833c1520e33f2736e9f48d0cd722b0482508824
SimonaSI <stoicasimona19@stud.ase.ro>
2022-03-02 18:56:36 +0200

    Add imbunatatiri

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,10 +13,12 @@ public class Main {
         Zebra zebra1 = new Zebra("zebra1");
         Zebra zebra2 = new Zebra("zebra2");
         Girafa girafa = new Girafa("girafa1");
+        Girafa chichi = new Girafa("chichi");
 
         zoo.addAnimal(zebra1);
         zoo.addAnimal(zebra2);
         zoo.addAnimal(girafa);
+        zoo.addAnimal(chichi);
 
         zoo.hranesteAnimale("iarba");
 
